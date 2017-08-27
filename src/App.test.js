89c7a234578e6fb7edf76ba02c7465dd35f9d399
Ross Allen <rossgit@gmail.com>
2017-08-27 12:41:21 +0800

    Add Flow types for Jest to flowify .test.js files

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
+/* @flow */
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
