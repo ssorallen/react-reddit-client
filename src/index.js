60a3385c922e3de7d76d53efd8e5302be0fe6ccf
Ross Allen <ross@driver.xyz>
2017-12-28 17:53:10 -0600

    Upgrade Flow and resolve warning
    * `document.getElementById` can return `null`; handle that case

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const root = document.getElementById('root')
+if (root == null) throw new Error('Missing #root element in the DOM');
+
+ReactDOM.render(<App />, root);
