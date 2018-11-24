5c7b1930b9d43cbc3e149553ec6e3f24fd552fd2
Ross Allen <rossgit@gmail.com>
2018-11-24 15:21:22 -0600

    Memoize sorted nav items to limit re-renders

--- src/Navigation.js
+++ src/Navigation.js
@@ -3,6 +3,7 @@ import './Navigation.css';
 import NavigationItem from './NavigationItem';
 import React from 'react';
 import { Subreddit } from './types';
+import memo from 'memoize-one';
 
 interface Props {
   activeUrl: string;
@@ -15,26 +16,28 @@ export default class Navigation extends React.Component<Props> {
     this.props.itemSelected(item);
   };
 
-  render() {
-    const items = this.props.items
-      .sort(
-        (a, b) =>
-          // Sort by # of subscribers in descending order
-          b.data.subscribers - a.data.subscribers
-      )
-      .map(item => (
-        <NavigationItem
-          item={item}
-          itemSelected={this.setSelectedItem}
-          key={item.data.id}
-          selected={item.data.url === this.props.activeUrl}
-        />
-      ));
+  sortedItems = memo(items =>
+    items.sort(
+      (a, b) =>
+        // Sort by # of subscribers in descending order
+        b.data.subscribers - a.data.subscribers
+    )
+  );
 
+  render() {
     return (
       <div className="navigation">
         <div className="header">Navigation</div>
-        <ul>{items}</ul>
+        <ul>
+          {this.sortedItems(this.props.items).map(item => (
+            <NavigationItem
+              item={item}
+              itemSelected={this.setSelectedItem}
+              key={item.data.id}
+              selected={item.data.url === this.props.activeUrl}
+            />
+          ))}
+        </ul>
       </div>
     );
   }
