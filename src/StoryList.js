930738e61a480ad7c9337042290063d1c64687ea
Ross Allen <rossgit@gmail.com>
2023-05-31 09:00:11 -0400

    Link to post author on reddit.com

--- src/StoryList.js
+++ src/StoryList.js
@@ -7,10 +7,6 @@ interface Props {
   items: Array<Story>;
 }
 
-// Because this component has no state and needs no instance methods, it is implemented as a
-// [stateless functional component][0], i.e. a plain JavaScript function.
-//
-// [0]: https://reactjs.org/docs/components-and-props.html#functional-and-class-components
 export default function StoryList(props: Props) {
   return (
     <table>
@@ -25,7 +21,8 @@ export default function StoryList(props: Props) {
                 <a href={item.data.url}>{item.data.title}</a>
               </p>
               <p className="author">
-                Posted by <span>{item.data.author}</span>
+                Posted by{" "}
+                <a href={`https://www.reddit.com/user/${item.data.author}`}>{item.data.author}</a>
               </p>
             </td>
           </tr>
